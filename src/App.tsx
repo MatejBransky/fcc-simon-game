9307bdde0bb926e48516a1da29506a8c26ef0a98
Matej Bransky <matej.bransky@gmail.com>
2019-03-09 00:54:01 +0100

    create basic CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useRef, useReducer } from 'react';
 
-import reducer, { initialState } from '../reducer';
-import './App.scss';
+import reducer, { initialState } from './reducer';
 
 const App = () => {
   const btnGreen = useRef(null);
@@ -12,32 +11,32 @@ const App = () => {
   const count = state.steps.length;
 
   return (
-    <div className="App">
-      <div className="App__base Game">
+    <div className="Wrapper">
+      <div className="Game">
         <div className="Game__buttons">
           <button
-            className="Game__button Game__button--top-left"
+            className="Game__button Game__button--top-left Game__button--green"
             data-testid="button-green"
             ref={btnGreen}
           />
           <button
-            className="Game__button Game__button--top-right"
+            className="Game__button Game__button--top-right Game__button--red"
             data-testid="button-red"
             ref={btnRed}
           />
           <button
-            className="Game__button Game__button--bottom-right"
+            className="Game__button Game__button--bottom-left Game__button--yellow"
             data-testid="button-yellow"
             ref={btnYellow}
           />
           <button
-            className="Game__button Game__button--bottom-left"
+            className="Game__button Game__button--bottom-right Game__button--blue"
             data-testid="button-blue"
             ref={btnBlue}
           />
         </div>
         <div className="Game__dashboard Dashboard">
-          <title className="Dashboard__title">Simon</title>
+          <header className="Dashboard__title">Simon</header>
 
           <div className="Dashboard__controls Controls">
             <div className="Controls__block Block">
@@ -46,28 +45,27 @@ const App = () => {
             </div>
 
             <div className="Controls__block Block">
-              <button className="Block__button" />
+              <button className="Block__button Block__button--red" />
               <p className="Block__description">Start</p>
             </div>
 
             <div className="Controls__block Block">
               <div className="Block__control" />
-              <button className="Block__button" />
+              <button className="Block__button Block__button--yellow" />
               <p className="Block__description">Strict</p>
             </div>
           </div>
 
-          <div className="Dashboard__switch Switch">
+          <div
+            className={
+              'Dashboard__switch Switch ' +
+              (state.on ? 'Switch--on' : 'Switch--off')
+            }
+          >
             <p className="Switch__text Switch__text--left">Off</p>
-            <div className="Switch__socket">
-              <button
-                className={
-                  'Switch__button ' + state.on
-                    ? 'Switch__button--on'
-                    : 'Switch__button--off'
-                }
-              />
-            </div>
+            <button className="Switch__socket">
+              <div className={'Switch__handle '} />
+            </button>
             <p className="Switch__text Switch__text--right">On</p>
           </div>
         </div>
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import './styles.scss';
-import App from './components/App';
+import App from './App';
 
 render(<App />, document.getElementById('app'));
